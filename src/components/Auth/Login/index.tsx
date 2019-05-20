f5c5af6ae6516f1e71a25b09642eb60aeaac1ee1
Chris Cedeno <chrisrey95@gmail.com>
2019-05-20 13:40:15 -0400

    Login and Register components now redirect on succesful attempt

--- src/components/Auth/Login/index.tsx
+++ src/components/Auth/Login/index.tsx
@@ -2,23 +2,31 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../actions';
 import Form from '../Form';
+import { IAppState } from '../../../store/Store';
+import { authReducer } from '../reducer';
+import { Redirect } from 'react-router';
 export interface IAppProps {
-  login: any
+  login: any,
+  jwt: boolean,
 }
 
 const Login = (props:IAppProps) =>  {
 
-  return (
-    <>
-      <h1>Login</h1>
-      <Form onSubmit={props.login}/>
-    </>
-  );
+  return props.jwt 
+    ? <Redirect to="/category"/>  
+    :(
+      <>
+        <h1>Login</h1>
+        <Form onSubmit={props.login}/>
+      </>
+    );
 }
+
+const mapStateToProps = (state:IAppState) => ({jwt: state.auth.jwt})
 
 // Object of action creators
 const mapDispatchToProps = {
   login,
 }
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/components/Auth/Register/index.tsx
+++ src/components/Auth/Register/index.tsx
@@ -2,24 +2,31 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { register } from '../actions';
 import Form from '../Form';
+import { IAppState } from '../../../store/Store';
+import { Redirect } from 'react-router';
 
 export interface IAppProps {
-  register: any
+  register: any,
+  jwt: boolean,
 }
 
 const Register = (props:IAppProps) =>  {
 
-  return (
-    <>
-        <h1>Register</h1>
-        <Form onSubmit={props.register}/>
-    </>
-  );
+  return props.jwt 
+    ? <Redirect to="/category"/>  
+    :(
+      <>
+          <h1>Register</h1>
+          <Form onSubmit={props.register}/>
+      </>
+    );
 }
+
+const mapStateToProps = (state:IAppState) => ({jwt: state.auth.jwt})
 
 // Object of action creators
 const mapDispatchToProps = {
   register,
 }
 
-export default connect(null, mapDispatchToProps)(Register);
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
